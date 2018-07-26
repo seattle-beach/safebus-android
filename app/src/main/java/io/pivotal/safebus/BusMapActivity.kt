93df4dba51bc444b7163e315280852bd01de48c9
Andres Medina <amedina@pivotal.io>
2018-07-25 17:14:08 -0700

    :rocket:

---
+++ app/src/main/java/io/pivotal/safebus/BusMapActivity.kt
@@ -0,0 +1,12 @@
+package io.pivotal.safebus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BusMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bus_map)
+    }
+}
