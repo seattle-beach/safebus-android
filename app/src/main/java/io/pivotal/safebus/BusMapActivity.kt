e876cfa4aee864c300bc6041d64468b3f888f31b
Andres Medina <amedina@pivotal.io>
2018-07-26 15:50:40 -0700

    test that location in map is enabled

--- app/src/main/java/io/pivotal/safebus/BusMapActivity.kt
+++ app/src/main/java/io/pivotal/safebus/BusMapActivity.kt
@@ -1,18 +1,28 @@
 package io.pivotal.safebus
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 class BusMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private val LOCATION_PERMISSION_CODE = 0
+    private val PIVOTAL_LOCATION = LatLng(47.5989794, -122.335976)
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,23 +31,58 @@ class BusMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_CODE)
+        } else {
+            enableLocation()
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+            val grantedLocationPermission = permissions
+                    .zip(grantResults.asIterable())
+                    .any { (permission, result) ->
+                        permission == Manifest.permission.ACCESS_FINE_LOCATION &&
+                                result == PackageManager.PERMISSION_GRANTED
+                    }
+            if (grantedLocationPermission) {
+                enableLocation()
+            } else {
+                updateCamera(PIVOTAL_LOCATION)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocation() {
+        map.isMyLocationEnabled = true
+        fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        val latlng = LatLng(location.latitude, location.longitude)
+                        updateCamera(latlng)
+                    } else {
+                        updateCamera(PIVOTAL_LOCATION)
+                    }
+                }
+    }
+
+    fun updateCamera(latlng: LatLng) {
+        val zoomUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15.00f)
+        map.animateCamera(zoomUpdate)
+    }
+
 }
--- app/src/test/java/io/pivotal/safebus/BusMapActivityTest.kt
+++ app/src/test/java/io/pivotal/safebus/BusMapActivityTest.kt
@@ -1,14 +1,38 @@
 package io.pivotal.safebus
 
+import android.Manifest
+import com.google.android.gms.maps.GoogleMap
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+
 
 @RunWith(RobolectricTestRunner::class)
 class BusMapActivityTest {
+    private lateinit var subject: BusMapActivity
+
+    @MockK
+    lateinit var mockMap: GoogleMap
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+
+        subject = Robolectric.setupActivity(BusMapActivity::class.java)
+        val shadowActivity = shadowOf(subject)
+        shadowActivity.grantPermissions(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        subject.onMapReady(mockMap)
+    }
+
     @Test
-    fun starts_activity() {
-        Robolectric.setupActivity(BusMapActivity::class.java)
+    fun location_gets_enabled() {
+        verify { mockMap.isMyLocationEnabled = true }
     }
 }
